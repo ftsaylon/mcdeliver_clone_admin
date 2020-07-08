22ce2f836628d267f268a730b65019af3da5c1dc
Francis Saylon <28182748+ftsaylon@users.noreply.github.com>
2020-07-08 20:00:44 +0800

    Redesigned auth screen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,91 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/services.dart';
 
 import '../widgets/auth_form.dart';
 
-class AuthScreen extends StatefulWidget {
-  AuthScreen({Key key}) : super(key: key);
+class AuthScreen extends StatelessWidget {
+  static const routeName = '/auth';
 
   @override
-  _AuthScreenState createState() => _AuthScreenState();
-}
-
-class _AuthScreenState extends State<AuthScreen> {
-  final _auth = FirebaseAuth.instance;
-  var _isLoading = false;
-
-  void _submitAuthForm(
-    String email,
-    String password,
-    String username,
-    bool isLogin,
-    BuildContext context,
-  ) async {
-    AuthResult authResult;
-
-    try {
-      setState(() {
-        _isLoading = true;
-      });
-
-      if (isLogin) {
-        authResult = await _auth.signInWithEmailAndPassword(
-          email: email,
-          password: password,
-        );
-      } else {
-        authResult = await _auth.createUserWithEmailAndPassword(
-          email: email,
-          password: password,
-        );
-
-        await FirebaseDatabase.instance.reference().child('users').push().set({
-          'userId': authResult.user.uid,
-          'firstName': '',
-          'lastName': '',
-          'email': email,
-          'address': '',
-        });
-      }
-    } on PlatformException catch (e) {
-      var message = 'An error occured, please check your credentials';
-
-      if (e.message != null) {
-        message = e.message;
-      }
-
-      Scaffold.of(context).showSnackBar(
-        SnackBar(
-          content: Text(message),
-          backgroundColor: Theme.of(context).errorColor,
-        ),
-      );
-
-      setState(() {
-        _isLoading = false;
-      });
-    } catch (e) {
-      setState(() {
-        _isLoading = false;
-      });
-
-      print(e);
-    }
-  }
-
-  @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     backgroundColor: Theme.of(context).primaryColor,
-  //     body: AuthForm(
-  //       submitFn: _submitAuthForm,
-  //       isLoading: _isLoading,
-  //     ),
-  //   );
-  // }
-
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
     return SafeArea(
@@ -124,10 +44,7 @@ class _AuthScreenState extends State<AuthScreen> {
                       ],
                     ),
                     SizedBox(height: 10),
-                    AuthForm(
-                      submitFn: _submitAuthForm,
-                      isLoading: _isLoading,
-                    ),
+                    AuthForm(),
                   ],
                 ),
               ),
